54c3b4e1aea67e54371bf67718fcae03ea72c6ca
Heroikk <68660405+Heroikk@users.noreply.github.com>
2020-08-24 23:07:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+//NIGGA U IDOT
+
+craftit = require('craftit');
+
+if (craftit = eat poop) {
+  craftit.idot_eating_poop_idiot_dumb_nigga_NIGgA = true
+  console.log('craftit.idot_eating_poop_idiot_dumb_nigga_NIGgA = true')
+} else {
+  craftit.idot_eating_poop_idiot_dumb_nigga_NIGgA = true
+  console.log('craftit.idot_eating_poop_idiot_dumb_nigga_NIGgA = true')
+}
